bd33a1c50005189d6fae66caad9853ee3d48ffc4
cocobin00 <cocobin000@naver.com>
2023-08-23 21:28:58 +0900

    값과 변수 & 데이터 타입

---
+++ app.js
@@ -0,0 +1,17 @@
+// value
+console.log(10);
+console.log("Cobin");
+
+// variable
+let firstName = "Thomas";
+firstName = "Alex";
+console.log(firstName);
+
+// camelCase
+let myAge = 10; // Good
+let MyAge = 12; // Bad
+let $myAge = 15; //Bad
+// let 1myAge = 20; // Bad
+
+// snake_case
+let my_age = 25;
---
+++ app2.js
@@ -0,0 +1,46 @@
+// data type
+
+// Primitive
+
+// 1. Number
+console.log(10);
+let num = 12;
+console.log(num);
+console.log(typeof num);
+
+// 2. String
+console.log("kevin");
+let name = "John";
+console.log(name);
+console.log(typeof name);
+
+// template literal (ES6)
+let myName = "kim";
+let templateLiteral = `hello ${myName}`;
+console.log(templateLiteral);
+
+// 3. Boolean
+console.log(true);
+let isFun = false;
+console.log(isFun);
+console.log(typeof isFun);
+
+// 4. Undefined
+let apple;
+console.log(apple);
+console.log(typeof apple);
+
+// 5. Null
+let time = null;
+console.log(time);
+console.log(typeof time);
+
+// 6. Symbol (ES6 = ES2015)
+let symbol = Symbol();
+console.log(symbol);
+console.log(typeof symbol);
+
+// 7. BigInt (ES2020)
+theBiggestInt = 9007199254343443;
+console.log(theBiggestInt);
+console.log(typeof theBiggestInt);
